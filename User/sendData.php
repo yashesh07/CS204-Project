f06775657e92c8f0ad28bc75e1e127b041169b22
Ishaan <aroraishaan2002@gmail.com>
2022-04-23 23:56:13 +0530

    galat sahi patan nhi

--- User/sendData.php
+++ User/sendData.php
@@ -12,17 +12,20 @@ $con=mysqli_connect($server,$username,$password);
    if(!$con){
        die("Connection to this database failed due to" . mysqli_connect_error());
    }
- $passenger_ID = $_POST['passenger_ID'];
-  $first_name = $_POST['first_name'];
-  $middle_name = $_POST['middle_name'];
-  $last_name = $_POST['last_name'];
-  $pincode = $_POST['pincode'];
-  $gender = $_POST['gender'];
-  $age = $_POST['age'];
-  $dob = $_POST['dob'];
-  $city = $_POST['city'];
-  $state = $_POST['state'];
-  $sql = "INSERT INTO `Railway System`.`passenger` (`passenger_ID`, `first_name`, `middle_name`, `last_name`, `pincode`, `city`, `state`,`dob`,`gender`,`age`) VALUES ('$passenger_ID', '$first_name', '$middle_name', '$last_name', '$pincode', '$city', '$state','$dob','$gender','$age');";
+  $passenger_ID=$_POST['passenger_ID'];
+  $first_name=$_POST['first_name'];
+//   $middle_name=$_POST['middle_name'];
+//   $name_last=$_POST['name_last'];
+//   $pincode=$_POST['pincode'];
+//   $city=$_POST['city'];
+//   $state=$_POST['state'];
+//   $dob=$_POST['dob'];
+//   $gender=$_POST['gender'];
+//   $age=$_POST['age'];
+//   $sql = "INSERT INTO `railway system`.`passenger` (`passenger_ID`, `first_name`, `middle_name`, `last_name`, `pincode`, `city`, `state`,`dob`,`gender`,`age`) VALUES ('$passenger_ID', '$first_name', '$middle_name', '$name_last', '$pincode', '$city', '$state','$dob','$gender','$age');";
+   $sql = "INSERT INTO 'railway system'.'passeger' ('passenger_ID', 'first_name') VALUES ('$passenger_ID','$first_name');";
+   echo $sql;
+
 //    echo $sql;
 //Execute the query
   if($con->query($sql) == true){
@@ -35,8 +38,7 @@ $con=mysqli_connect($server,$username,$password);
   }
   //Close the database connection
   $con->close();
-
-
+}
 ?>
 
 <!DOCTYPE html>
@@ -52,19 +54,20 @@ $con=mysqli_connect($server,$username,$password);
         if($insert==true){
         echo "<p class='confirmation-text'>Thanks for submitting the form.We hope to see you onboard with us on the US Trip.</p>";
     }
-        ?>
+    ?>
     <div>
      <form action="sendData.php" method="post">
-    <input type="text" name="name" id="name" placeholder="Enter your first name">
-     <input type="text" name="name" id="name" placeholder="Enter your middle name">
-     <input type="text" name="name" id="name" placeholder="Enter your last name">
-            <input type="text" name="age" id="age" placeholder="Enter your Age">
-            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
-            <input type="date" name="date" id="date" placeholder="Enter your date of birth">
-            <input type="date" name="date" id="date" placeholder="Enter your date">
-            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="ENter some other descripyion">
+       <input type="text" id="passenger_ID" name="passenger_ID">
+      <input type="text" id="first_name" name="first_name">
+      <!-- <input type="text" id="middle_name">
+      <input type="text" id="pincode">
+      <input type="text" id="city">
+      <input type="text" id="state">
+      <input type="date" id="dob">
+      <input type="text" id="gender">
+      <input type="text" id="age"> -->
+      <!-- <input type="text" id="name_last"> -->
 
-            </textarea>
      <button class="btn btn-outline-light btn-lg">Submit</button>
      </form>
      </div>
